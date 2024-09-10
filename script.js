23446a9eb16e7f66e0cdf2ba013136fac7e2fa93
julianll20 <julianllera20@gmail.com>
2024-09-10 13:01:06 -0300

    Actualizado

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let sections = document.querySelectorAll("section");
-  let navLinks = document.querySelectorAll("header nav a");
 
-  window.addEventListener("scroll", function() {
-    let scrollY = window.scrollY;
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupal");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
 
-    sections.forEach((sec) => {
-      let top = sec.offsetTop - 150;
-      let bottom = top + sec.offsetHeight;
-      let id = sec.getAttribute("id");
 
-      if (scrollY >= top && scrollY < bottom) {
-        navLinks.forEach((link) => {
-          if (link.getAttribute("href") === `#${id}`) {
-            link.classList.add("active");
-          } else {
-            link.classList.remove("active");
-          }
-        });
-      }
-    });
-
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', scrollY > 100);
-  });
-});
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
